955b09f0cd64c134e38aa8f9128e9dfaec8c27eb
ZephyrFuckU <ortmann.ole@gmail.com>
2024-04-05 13:08:20 +0200

    drfgvsdf

--- UE_DungeonCompany/Intermediate/Build/Win64/x64/UE_DungeonCompanyEditor/Development/Core/SharedPCH.Core.Cpp20.cpp
+++ UE_DungeonCompany/Intermediate/Build/Win64/x64/UE_DungeonCompanyEditor/Development/Core/SharedPCH.Core.Cpp20.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.34.31946
+// Compiler: 14.36.32538
 #include "C:/Users/oleor/Documents/GitHub/UniStuff/Projects/DungeonCompany/UE_DungeonCompany/Intermediate/Build/Win64/x64/UE_DungeonCompanyEditor/Development/Core/SharedPCH.Core.Cpp20.h"
--- UE_DungeonCompany/Source/UE_DungeonCompany/UE_DungeonCompany.Build.cs
+++ UE_DungeonCompany/Source/UE_DungeonCompany/UE_DungeonCompany.Build.cs
@@ -8,7 +8,7 @@ public class UE_DungeonCompany : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsytem", "OnlineSubsystemSteam" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
