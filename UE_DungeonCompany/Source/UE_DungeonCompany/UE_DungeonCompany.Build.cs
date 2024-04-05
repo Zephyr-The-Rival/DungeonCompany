2dc1119c543d77ef20838aa5670869dabd6f707d
ZephyrFuckU <ortmann.ole@gmail.com>
2024-04-05 12:45:38 +0200

    errors in VS before i even started anything. great!

--- UE_DungeonCompany/Source/UE_DungeonCompany/UE_DungeonCompany.Build.cs
+++ UE_DungeonCompany/Source/UE_DungeonCompany/UE_DungeonCompany.Build.cs
@@ -8,7 +8,7 @@ public class UE_DungeonCompany : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsytem", "OnlineSubsystemSteam" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
