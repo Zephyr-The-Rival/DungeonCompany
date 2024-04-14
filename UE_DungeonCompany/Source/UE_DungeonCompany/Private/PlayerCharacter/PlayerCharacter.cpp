e6716b1667cc894d2e0daf8636dfe5bd11c1a175
ZephyrFuckU <ortmann.ole@gmail.com>
2024-04-14 15:40:14 +0200

    no auto posess

--- UE_DungeonCompany/Source/UE_DungeonCompany/Private/PlayerCharacter/PlayerCharacter.cpp
+++ UE_DungeonCompany/Source/UE_DungeonCompany/Private/PlayerCharacter/PlayerCharacter.cpp
@@ -10,7 +10,7 @@ APlayerCharacter::APlayerCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	AutoPossessPlayer = EAutoReceiveInput::Player0;
+	//AutoPossessPlayer = EAutoReceiveInput::Player0;
 
 	bUseControllerRotationYaw = false;//nessecary?
 
