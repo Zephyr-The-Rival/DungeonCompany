9e16e81321c7c8c9f53b0f0c3517828501de79ed
ZephyrFuckU <ortmann.ole@gmail.com>
2024-04-19 13:23:26 +0200

    Created world item and item data

---
+++ UE_DungeonCompany/Source/UE_DungeonCompany/Public/Items/ItemData.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ItemData.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class UE_DUNGEONCOMPANY_API UItemData : public UObject
+{
+	GENERATED_BODY()
+	
+//public:
+//	UItemData();
+//
+//protected:
+//
+//	FString Name;
+//	UTexture2D Icon;
+
+};
