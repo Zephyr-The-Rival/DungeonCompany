19076f2af7a9c0c8eee79ede6b30268086585ce7
Timon Albrecht <timon-albrecht@outlook.de>
2024-04-26 15:31:51 +0200

    Voice cvars setters

--- UE_DungeonCompany/Source/UE_DungeonCompany/Private/DC_Statics.cpp
+++ UE_DungeonCompany/Source/UE_DungeonCompany/Private/DC_Statics.cpp
@@ -2,3 +2,15 @@
 
 
 #include "DC_Statics.h"
+
+void UDC_Statics::SetMicInputGain(float Value)
+{
+	static IConsoleVariable* micInputGain = IConsoleManager::Get().FindConsoleVariable(TEXT("voice.MicInputGain"));
+	micInputGain->Set(Value);
+}
+
+void UDC_Statics::SetMicNoiseGateTreshold(float Value)
+{
+	static IConsoleVariable* micNoiseGateTreshold = IConsoleManager::Get().FindConsoleVariable(TEXT("voice.MicNoiseGateThreshold"));
+	micNoiseGateTreshold->Set(Value);
+}
--- UE_DungeonCompany/Source/UE_DungeonCompany/Public/DC_Statics.h
+++ UE_DungeonCompany/Source/UE_DungeonCompany/Public/DC_Statics.h
@@ -4,14 +4,22 @@
 
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
 #include "DC_Statics.generated.h"
 
 #define LogWarning(Message) UE_LOG(LogTemp, Warning, TEXT("%s"),Message)
 #define PrintMessage(Message, DisplayLength) GEngine->AddOnScreenDebugMessage(-1, DisplayLength, FColor::Green, Message)
 
 UCLASS()
-class UE_DUNGEONCOMPANY_API UDC_Statics : public UObject
+class UE_DUNGEONCOMPANY_API UDC_Statics : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="Microphone")
+	static void SetMicInputGain(float Value);
+
+	UFUNCTION(BlueprintCallable, Category="Microphone")
+	static void SetMicNoiseGateTreshold(float Value);
 	
 };
