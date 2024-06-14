0458dac42170d705f71869544881b7c9c0b9f638
Timon Albrecht <timon-albrecht@outlook.de>
2024-06-14 14:57:49 +0200

    Additional ToggleInventory Input in Inventory IMC

--- UE_DungeonCompany/Source/UE_DungeonCompany/Public/PlayerCharacter/PlayerCharacter.h
+++ UE_DungeonCompany/Source/UE_DungeonCompany/Public/PlayerCharacter/PlayerCharacter.h
@@ -93,6 +93,9 @@ private:
 	UInputMappingContext* InventoryInputMapping;
 
 	UPROPERTY(EditAnywhere, Category = "Input | Action")
+	UInputAction* ToggleInventoryInvAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input | Action")
 	UInputAction* NavigateInventoryAction;
 
 	UPROPERTY(EditAnywhere, Category = "Input | Action")
