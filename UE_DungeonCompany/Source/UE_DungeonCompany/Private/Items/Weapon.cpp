651682d59a0595de58678959731afa0d19b9b63e
Ole Ortmann <ortmann.ole@gmail.com>
2024-07-28 18:17:00 +0200

    character cant hit themselves

--- UE_DungeonCompany/Source/UE_DungeonCompany/Private/Items/Weapon.cpp
+++ UE_DungeonCompany/Source/UE_DungeonCompany/Private/Items/Weapon.cpp
@@ -49,8 +49,12 @@ void AWeapon::DealHits(FWeaponInfo WeaponInfo)
 	
 	if(ADC_Entity* hitEntity= Cast<ADC_Entity>(HitActor))
 	{
-		hitEntity->TakeDamage(10);
-		hitEntity->SpawnHitEffect(Cast<USceneComponent>(hitResult.GetComponent()), hitResult.BoneName, hitResult.Location, TraceEnds[i] - TraceStarts[i]);	
+		if(hitEntity!=MyCharacterToAttachTo)
+		{
+			hitEntity->TakeDamage(10);
+			hitEntity->SpawnHitEffect(Cast<USceneComponent>(hitResult.GetComponent()), hitResult.BoneName, hitResult.Location, TraceEnds[i] - TraceStarts[i]);
+		}
+			
 	}
 
 	if (ABreakableProp* Prop = Cast<ABreakableProp>(HitActor))
