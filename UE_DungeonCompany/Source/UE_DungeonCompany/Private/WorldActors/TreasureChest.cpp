d43a0999e7ba376387363bb73efaaa8c245565aa
Zephyr <105359368+Zephyr-The-Rival@users.noreply.github.com>
2024-08-08 18:33:35 +0200

    treasure chest

---
+++ UE_DungeonCompany/Source/UE_DungeonCompany/Private/WorldActors/TreasureChest.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldActors/TreasureChest.h"
+
+#include "PlayerCharacter/PlayerCharacter.h"
+#include "UI/PlayerHud/PlayerHud.h"
+
+ATreasureChest::ATreasureChest()
+{
+	this->bInteractOnServer=true;
+}
+
+void ATreasureChest::OnHovered(APlayerCharacter* PlayerCharacter)
+{
+	if(!bUsed)
+	{
+		IInteractable::OnHovered(PlayerCharacter);
+		PlayerCharacter->GetMyHud()->ShowTextInteractPrompt("Open");
+	}
+	
+}
+
+void ATreasureChest::AuthorityInteract(APawn* InteractingPawn)
+{
+	if(!bUsed)
+	{
+		OnInteractedOnServer();
+		bUsed=true;
+	}
+	
+}
+
+void ATreasureChest::OnInteractedOnServer_Implementation()
+{
+
+}
---
+++ UE_DungeonCompany/Source/UE_DungeonCompany/Public/WorldActors/TreasureChest.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactable.h"
+#include "UObject/Object.h"
+#include "TreasureChest.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class UE_DUNGEONCOMPANY_API ATreasureChest : public AActor, public IInteractable
+{
+	GENERATED_BODY()
+
+	ATreasureChest();
+
+protected:
+
+	virtual void OnHovered(APlayerCharacter* PlayerCharacter) override;
+	virtual void AuthorityInteract(APawn* InteractingPawn) override;
+
+	UFUNCTION(BlueprintNativeEvent)
+	void OnInteractedOnServer();
+	void OnInteractedOnServer_Implementation();
+
+	bool bUsed=false;
+	
+
+	
+};
