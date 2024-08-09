d488b6e12b478dbacf7b1012a3bcf7584d90d7d5
Zephyr <105359368+Zephyr-The-Rival@users.noreply.github.com>
2024-08-09 22:25:23 +0200

    loading screen

--- UE_DungeonCompany/Source/UE_DungeonCompany/Private/Items/WorldItem.cpp
+++ UE_DungeonCompany/Source/UE_DungeonCompany/Private/Items/WorldItem.cpp
@@ -17,7 +17,7 @@ AWorldItem::AWorldItem()
 	PrimaryActorTick.bCanEverTick = true;
 	bReplicates = true;
 	bAlwaysRelevant = true;
-
+	bAttachesToRightHand=true;
 
 	static ConstructorHelpers::FObjectFinder<USoundBase> basicSound(TEXT("/Game/_DungeonCompanyContent/Audio/PickUpSounds/PickUpGeneric"));
 	this->PickUpSound = basicSound.Object;
@@ -105,13 +105,12 @@ void AWorldItem::AttachToPlayer()
 
 void AWorldItem::Interact(APawn* InteractingPawn)
 {
-	LogWarning(*(this->GetName()+" is beeing interacted with"));
 
 	APlayerCharacter* character = Cast<APlayerCharacter>(InteractingPawn);
 
 	if(!character)
 		return;
-
+	
 	character->PickUpItem(this);
 }
 
