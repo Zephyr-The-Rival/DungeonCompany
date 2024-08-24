27a0da7a8da17690bb5dd04d55eb53ce775a8134
Zephyr <105359368+Zephyr-The-Rival@users.noreply.github.com>
2024-08-25 00:06:58 +0200

    sending stone

---
+++ UE_DungeonCompany/Source/UE_DungeonCompany/Public/Items/SendingStone.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WorldItem.h"
+#include "SendingStone.generated.h"
+
+enum class ESendingStoneAnimatoinState : uint8;
+
+UCLASS()
+class UE_DUNGEONCOMPANY_API ASendingStone : public AWorldItem
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ASendingStone();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+
+	virtual void TriggerLocalPrimaryAction_Implementation(APlayerCharacter* User) override;
+	
+	virtual void EndLocalPrimaryActionHold_Implementation(APlayerCharacter* User) override;
+
+public:
+	UFUNCTION(BlueprintNativeEvent)
+	void Server_SendSignal(ESendingStoneAnimatoinState Signal);
+	void Server_SendSignal_Implementation(ESendingStoneAnimatoinState Signal);
+};
