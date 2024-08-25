0035f0c40c59e5b7ea8a9644ad8aa44fa82676ac
Zephyr <105359368+Zephyr-The-Rival@users.noreply.github.com>
2024-08-25 15:37:09 +0200

    starting class switcher

---
+++ UE_DungeonCompany/Source/UE_DungeonCompany/Private/WorldActors/ClassSwitcher.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldActors/ClassSwitcher.h"
+
+
+// Sets default values
+AClassSwitcher::AClassSwitcher()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AClassSwitcher::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AClassSwitcher::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AClassSwitcher::OnHovered(APlayerCharacter* PlayerCharacter)
+{
+	Super::OnHovered(PlayerCharacter);
+	this->InteractPromptText="SwichToMercenary";
+}
+
---
+++ UE_DungeonCompany/Source/UE_DungeonCompany/Public/WorldActors/ClassSwitcher.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasicButton.h"
+#include "GameFramework/Actor.h"
+#include "ClassSwitcher.generated.h"
+
+UCLASS()
+class UE_DUNGEONCOMPANY_API AClassSwitcher : public ABasicButton
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AClassSwitcher();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+
+	void OnHovered(APlayerCharacter* PlayerCharacter) override;
+};
