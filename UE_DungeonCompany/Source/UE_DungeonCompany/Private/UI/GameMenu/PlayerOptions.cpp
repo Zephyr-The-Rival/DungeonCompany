2f7066bebdd2f39323d6a78e41bc64d44afd3c73
Timon Albrecht <timon-albrecht@outlook.de>
2024-08-27 21:21:00 +0200

    Player Volume Setting

--- UE_DungeonCompany/Source/UE_DungeonCompany/Private/UI/GameMenu/PlayerOptions.cpp
+++ UE_DungeonCompany/Source/UE_DungeonCompany/Private/UI/GameMenu/PlayerOptions.cpp
@@ -3,3 +3,28 @@
 
 #include "UI/GameMenu/PlayerOptions.h"
 
+#include "UI/Common/CommonValueChanger.h"
+#include "Kismet/GameplayStatics.h"
+#include "Subsystems/VoiceChatSubsystem.h"
+#include "Sound/SoundMix.h"
+
+UPlayerOptions::UPlayerOptions()
+{
+	static ConstructorHelpers::FObjectFinder<USoundMix> MasterSoundMixFinder(
+		TEXT("/Script/Engine.SoundMix'/Game/_DungeonCompanyContent/Audio/MasterSoundMix.MasterSoundMix'"));
+	MasterSoundMix = MasterSoundMixFinder.Object;
+}
+
+void UPlayerOptions::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	VolumeSlider->OnValueChangedEvent.AddDynamic(this, &UPlayerOptions::OnVolumeSliderValueChanged);
+}
+
+void UPlayerOptions::OnVolumeSliderValueChanged(float NewValue)
+{
+	USoundClass* voiceClass = GetGameInstance()->GetSubsystem<UVoiceChatSubsystem>()->GetVoiceClassForPlayer(
+		ControllingPlayer);
+	UGameplayStatics::SetSoundMixClassOverride(GetWorld(), MasterSoundMix, voiceClass, NewValue * 0.01);
+}
--- UE_DungeonCompany/Source/UE_DungeonCompany/Public/UI/GameMenu/PlayerOptions.h
+++ UE_DungeonCompany/Source/UE_DungeonCompany/Public/UI/GameMenu/PlayerOptions.h
@@ -6,15 +6,30 @@
 #include "CommonActivatableWidget.h"
 #include "PlayerOptions.generated.h"
 
-/**
- * 
- */
+class UCommonValueChanger;
+class USoundMix;
+
 UCLASS()
 class UE_DUNGEONCOMPANY_API UPlayerOptions : public UCommonActivatableWidget
 {
 	GENERATED_BODY()
 
+protected:
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, meta = (BindWidget))
+	UCommonValueChanger* VolumeSlider;
+
 private:
 	APlayerState* ControllingPlayer;
+	USoundMix* MasterSoundMix;
+
+public:
+	UPlayerOptions();
+
+protected:
+	virtual void NativeConstruct() override;
+
+protected:
+	UFUNCTION()
+	void OnVolumeSliderValueChanged(float NewValue);
 	
 };
