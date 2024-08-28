0314980225383f9e30a954788d7adf53351d674f
Timon Albrecht <timon-albrecht@outlook.de>
2024-08-28 19:46:45 +0200

    fixed money not replicating

--- UE_DungeonCompany/Source/UE_DungeonCompany/Private/WorldActors/SharedStatsManager.cpp
+++ UE_DungeonCompany/Source/UE_DungeonCompany/Private/WorldActors/SharedStatsManager.cpp
@@ -13,6 +13,7 @@ ASharedStatsManager::ASharedStatsManager()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	this->bReplicates = true;
+	bAlwaysRelevant = true;
 }
 
 // Called when the game starts or when spawned
